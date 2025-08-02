55ecdf0632e107eff4d2ddb81919cb28100606ca
bhupali <bhupali.sarma@ubona.com>
2025-08-02 20:54:26 +0530

    Added pingpong

---
+++ src/main/java/com/project/queueapp/QueueappApplication.java
@@ -0,0 +1,13 @@
+package com.project.queueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/queueapp/QueueappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.queueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
